5176a69dad00173fcd1258b507aafed339918fff
jcg <juanky.garcia15@gmail.com>
2025-01-16 16:16:06 -0500

    Add more debugger logic

--- src/debugger.ts
+++ src/debugger.ts
@@ -101,6 +101,37 @@ export class TxtDebugConfigurationProvider implements vscode.DebugConfigurationP
 export class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
 
     createDebugAdapterDescriptor(_session: vscode.DebugSession): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
-        return new vscode.DebugAdapterInlineImplementation(new CustomDebugSession());
+        return new vscode.DebugAdapterInlineImplementation(new CustomDebugSession(workspaceFileAccessor));
     }
+}
+
+export interface FileAccessor {
+	isWindows: boolean;
+	readFile(path: string): Promise<Uint8Array>;
+	writeFile(path: string, contents: Uint8Array): Promise<void>;
+}
+
+export const workspaceFileAccessor: FileAccessor = {
+	isWindows: typeof process !== 'undefined' && process.platform === 'win32',
+	async readFile(path: string): Promise<Uint8Array> {
+		let uri: vscode.Uri;
+		try {
+			uri = pathToUri(path);
+		} catch (e) {
+			return new TextEncoder().encode(`cannot read '${path}'`);
+		}
+
+		return await vscode.workspace.fs.readFile(uri);
+	},
+	async writeFile(path: string, contents: Uint8Array) {
+		await vscode.workspace.fs.writeFile(pathToUri(path), contents);
+	}
+};
+
+function pathToUri(path: string) {
+	try {
+		return vscode.Uri.file(path);
+	} catch (e) {
+		return vscode.Uri.parse(path);
+	}
 }
